5aa9fee4486c16eef8609829402c7b68f7a06511
bryan <bryanguillenlds@gmail.com>
2023-08-08 22:14:41 -0600

    Using React.memo to memoize entire component and not just function inside, like it was done with useMemo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Header = () => (
-  <h1>RoboFriends</h1>
-)
+const Header = () => {
+  return <h1>RoboFriends</h1>;
+};
 
-export default Header;
+export default React.memo(Header);
